da9fe59b01defe1945f570deee267b18d4bb6ca4
Sergey Shulga <shul.sergio@gmail.com>
2024-06-27 23:31:30 +0300

    v.3.964 add all stage playoffs
    v.3.964 add all stage playoffs

--- js/playoffs.js
+++ js/playoffs.js
@@ -2,6 +2,10 @@ import { onTopScoresList } from "./function.js";
 import { onCreateDataFromJson } from "./function.js";
 let itemStage = document.querySelector(".js-stage-playoff");
 const dropdown = document.querySelector("#options");
+const teamFromTableList = document.querySelector(".js-team-name");
+const LS_KEY_TEAM = "Team";
+// const LS_KEY_GROUP = "Group";
+teamFromTableList.addEventListener("click", onCheckClick);
 
 onSelectStageData();
 updateHtmlData("1/8");
@@ -15,6 +19,16 @@ itemStage.addEventListener("click", (evt) => {
 });
 
 // ----------functions--------
+
+function onCheckClick(evt) {
+  let TeamText = "";
+  if (evt.target.classList.contains("js-team-add")) {
+    TeamText = evt.target.textContent;
+    localStorage.setItem(LS_KEY_TEAM, JSON.stringify(TeamText));
+    location.href = "./main-matches.html";
+  }
+}
+
 /**
  * onSelectStageData() - func for transit
  *  between Play-offs <-> Main stage
@@ -43,7 +57,9 @@ async function onCreateHtmlData(chooseStage) {
     .map((item) => {
       return `<div class="playoff-list">
       <p class="playoffs-text-style data-playoff">${item.HomeTeamStatus}</p>
-              <p class="playoffs-text-style name-playoff">${item.HomeTeam}</p>
+              <p class="playoffs-text-style name-playoff js-team-add">${
+                item.HomeTeam
+              }</p>
               <p class="playoffs-text-style data-playoff">|</p>
               <p class="playoffs-text-style goal-playoff">
               ${
@@ -53,14 +69,16 @@ async function onCreateHtmlData(chooseStage) {
               }</p>
             </div>
             <div class="playoff-list">
-            <p class="playoffs-text-style data-playoff">${
-              item.AwayTeamStatus
-            }</p>
-              <p class="playoffs-text-style name-playoff">${item.AwayTeam}</p>
+            <p class="playoffs-text-style data-playoff">${item.AwayTeamStatus}
+              </p>
+              <p class="playoffs-text-style name-playoff js-team-add">${
+                item.AwayTeam
+              }</p>
               <p class="playoffs-text-style data-playoff">|</p>
               <p class="playoffs-text-style goal-playoff">${
                 item.AwayTeamScore === null ? "" : item.AwayTeamScore
-              }</p>
+              }
+                </p>
             </div>`;
     })
     .join("");
